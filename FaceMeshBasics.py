2ff8b605f6ca28df5b70380418e5bc86dff67cf9
victor <victor.lvl47@gmail.com>
2021-08-25 16:06:48 -0600

    Display FPS

--- FaceMeshBasics.py
+++ FaceMeshBasics.py
@@ -3,9 +3,16 @@ import mediapipe as mp
 import time 
 
 cap = cv2.VideoCapture('videos/1.mp4')
+pTime = 0
 
 while True:
     success, img = cap.read()
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, f'FPS: {int(fps)}', (20, 135), 
+        cv2.FONT_HERSHEY_PLAIN, 10, (0, 255, 0), 10)
 
     # resize img
     resized_img = cv2.resize(img, (640, 480))
