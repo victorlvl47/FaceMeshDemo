d71c3922168fbdd639e1508062ed6c90e2582d9b
victor <victor.lvl47@gmail.com>
2021-08-25 16:55:33 -0600

    Draw face mesh

--- FaceMeshBasics.py
+++ FaceMeshBasics.py
@@ -2,11 +2,25 @@ import cv2
 import mediapipe as mp
 import time 
 
-cap = cv2.VideoCapture('videos/1.mp4')
+cap = cv2.VideoCapture('videos/6.mp4')
 pTime = 0
+
+mpDraw = mp.solutions.drawing_utils
+mpFaceMesh = mp.solutions.face_mesh
+faceMesh = mpFaceMesh.FaceMesh(max_num_faces=1)
+drawSpec = mpDraw.DrawingSpec(thickness=2, circle_radius=2)
 
 while True:
     success, img = cap.read()
+
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = faceMesh.process(imgRGB)
+
+    if results.multi_face_landmarks:
+        for faceLms in results.multi_face_landmarks:
+            mpDraw.draw_landmarks(img, faceLms, 
+                    mpFaceMesh.FACEMESH_CONTOURS, drawSpec, 
+                    drawSpec)
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
